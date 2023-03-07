bc8b5d31aeeeaa3d164e437a28eccae10e03a0a4
James Christian Tarog <jftarog@student.apc.edu.ph>
2023-03-07 14:16:50 +0800

    fixed tab icon not showing on some pages

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -2,7 +2,7 @@
 <html lang="en-US">
 <head>
     <title>JC's Page</title>
-    <link rel="icon" type="image/x-icon" href="images/aonahara.ico">
+    <link rel="icon" type="image/x-icon" href="<?php echo base_url()?>/images/aonahara.ico">
     <?php echo link_tag('css/styles.css'); ?>
     <?php echo script_tag('js/script.js'); ?>
     <!-- <script src="../js/script.js"></script> -->
